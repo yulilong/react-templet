f8e7d87dc8821cba0e3367273da4e20465e9a89f
yulilong <yulilong222q@outlook.com>
2019-11-11 23:28:05 +0800

    添加antd UI包。

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Button} from 'antd';
 import './home.less'
 
 export default class Home extends React.Component {
 
   render () {
     return (
-      <div className="home">我是home</div>
+      <div className="home">
+        <div>
+            <Button type="primary">Primary</Button>
+            <Button>Default</Button>
+            <Button type="dashed">Dashed</Button>
+            <Button type="danger">Danger</Button>
+            <Button type="link">Link</Button>
+        </div>
+      </div>
     )
   }
 }
